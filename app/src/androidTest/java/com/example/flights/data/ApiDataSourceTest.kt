9b7d621d0f9647af0f10ccc955cca69b8e80d684
fabio.belliero <fabio.belliero@studenti.unimi.it>
2022-09-18 14:13:37 +0200

    API Call Testing

---
+++ app/src/androidTest/java/com/example/flights/data/ApiDataSourceTest.kt
@@ -0,0 +1,43 @@
+package com.example.flights.data
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
+import org.json.JSONArray
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+class TestCallback(): VolleyCallback{
+    override fun onSuccess(flightList: JSONArray) {
+        assertThat(flightList).isNotNull()
+        assertThat(flightList).isInstanceOf(JSONArray::class.java)
+    }
+
+    override fun onError() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+@RunWith(AndroidJUnit4::class)
+class ApiDataSourceTest {
+
+    private lateinit var apiDataSource: ApiDataSource
+    private lateinit var testCallback: TestCallback
+
+    @Before
+    fun setup(){
+        apiDataSource = ApiDataSource(ApplicationProvider.getApplicationContext())
+        testCallback = TestCallback()
+    }
+
+    @Test
+    fun apiTestResponse() = runTest {
+
+        apiDataSource.getFlightsList(testCallback)
+
+    }
+
+}
