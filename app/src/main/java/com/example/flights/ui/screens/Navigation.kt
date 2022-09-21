d50a160def55092e234f57d3fc6b4f271d61e5a8
fabio.belliero <fabio.belliero@studenti.unimi.it>
2022-09-21 16:10:17 +0200

    Added animations

--- app/src/main/java/com/example/flights/ui/screens/Navigation.kt
+++ app/src/main/java/com/example/flights/ui/screens/Navigation.kt
@@ -1,19 +1,36 @@
 package com.example.flights.ui.screens
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.flights.vm.MainViewModel
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigation(viewModel: MainViewModel){
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.Home.route){
-        composable(route = Screen.Home.route){
+    val navController = rememberAnimatedNavController()
+    AnimatedNavHost(navController = navController, startDestination = Screen.Home.route){
+        composable(
+            route = Screen.Home.route
+        ){
             HomeScreen(navController, viewModel)
         }
-        composable(route = Screen.Detail.route){
+        composable(route = Screen.Detail.route,
+            enterTransition = {
+                when (targetState.destination.route){ Screen.Detail.route ->
+                    slideInHorizontally(
+                        initialOffsetX = {300},
+                        animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing)
+                    ) + fadeIn(animationSpec = tween(300))
+                    else -> null
+                }
+            },
+            ){
             DetailScreen(navController, viewModel)
         }
     }
